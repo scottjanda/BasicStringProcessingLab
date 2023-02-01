6f6875b179ca61b5d45fb9711e359c646ee794c4
Scott <scottjanda@gmail.com>
2023-01-31 19:27:28 -0500

    Initial commit

---
+++ BasicStringProcessingLab/Program.cs
@@ -0,0 +1,56 @@
+﻿
+
+//Prompt the user to enter a sentence.  Split the sentence into individual words and display each word on its own line.
+
+
+string userContinue = "";
+
+do
+{
+
+    Console.WriteLine("Please enter a sentence:");
+    string userInput = Console.ReadLine();
+    Console.WriteLine();
+
+    List<string> list = new(userInput.Split(" "));
+
+    foreach (var item in list)
+    {
+        Console.WriteLine(item);
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("Do you want to continue (y/n)?");
+    userContinue = Console.ReadLine();
+    Console.WriteLine();
+
+
+} while (userContinue.ToLower() == "y");
+
+
+
+//Repeatedly prompt the user to enter a string.  Store the string in a list and display the contents of the list with each element separated by a space.
+
+List<string> list2 = new();
+
+do
+{
+
+    Console.WriteLine("Please enter a string:");
+    string userInput = Console.ReadLine();
+    Console.WriteLine();
+
+    list2.Add(userInput);
+
+
+    Console.WriteLine("Do you want to add another string (y/n)?");
+    userContinue = Console.ReadLine();
+    Console.WriteLine();
+
+} while (userContinue.ToLower() == "y");
+
+foreach (var item in list2)
+{
+    Console.Write(item + " ");
+}
+Console.WriteLine();
